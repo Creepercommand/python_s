9c9ded4d02bbe62d5531dacfd076f1aeb315592d
creepercommand07 <creepercommand07@gmail.com>
2020-02-15 16:02:59 +0900

    로또 번호 추출기

--- lotto.py
+++ lotto.py
@@ -7,14 +7,28 @@ def dislable():
     lotto_list = list(range(1, 46))
     random.shuffle(lotto_list)
     lbl.configure(text = lotto_list[:6])
+    lbl1.configure(text=lotto_list[6:12])
+    lbl2.configure(text=lotto_list[12:18])
+    lbl3.configure(text=lotto_list[18:24])
+    lbl4.configure(text=lotto_list[24:30])
+
 
 root = tk.Tk()
-root.geometry("200x100")
+root.geometry("200x200")
 
 lbl = tk.Label(text = "")
+lbl1 = tk.Label(text = "")
+lbl2 = tk.Label(text = "")
+lbl3 = tk.Label(text = "")
+lbl4 = tk.Label(text = "")
+
 btn = tk.Button(text = "클릭", command = dislable)
 
 lbl.pack()
+lbl1.pack()
+lbl2.pack()
+lbl3.pack()
+lbl4.pack()
 btn.pack()
 tk.mainloop()
 
